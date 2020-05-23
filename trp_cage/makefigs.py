f45e7438c27aeaa3b29294e359763464fb882b94
lewis <lewis.martin44@gmail.com>
2020-05-23 21:34:56 +1000

    trpcage figs

---
+++ trp_cage/makefigs.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+import mdtraj as md
+import numpy as np
+reference_pdb = md.load_pdb('./nmr_struc_1.pdb')
+
+
+standard_traj = md.load_dcd('./trp_standardmd_traj.dcd', top='./trp-min.pdb')
+#gst_equil_traj = md.load_dcd('./trp_gst_equilibration.dcd', top='./trp-min.pdb')
+gst_traj =  md.load_dcd('./trp_gst_traj.dcd', top='./trp-min.pdb')
+
+
+rmsd_smd = md.rmsd(standard_traj, reference_pdb)
+#rmsd = md.rmsd(gst_equil_traj, reference_pdb)
+rmsd_gst = md.rmsd(gst_traj, reference_pdb)
+
+
+fig, ax = plt.subplots()
+fig.set_figheight(5)
+fig.set_figwidth(10)
+
+time = np.linspace(0, len(rmsd_smd)*50000/500000,len(rmsd_smd) )
+ax.plot(time, rmsd_gst, label='Serial tempering')
+ax.plot(time, rmsd_smd, label='Standard MD')
+ax.set_ylabel('RMSD')
+ax.set_xlabel('Time (ns)')
+ax.legend()
+
+
+fig.savefig('trpcage.tif')
+fig.savefig('trpcage.svg')
+fig.savefig('trpcage.png')
